62e7e7c66017acae116b2f7b7275b705709bdcdf
Elisag28 Gonzalez <elisagonzalez281296@gmail.com>
2024-02-04 19:39:31 +0000

    streamlit

--- src/app.py
+++ src/app.py
@@ -2,19 +2,23 @@ import streamlit as st
 import pickle
 import pandas as pd
 
-# model
+# load model
 with open('../models/RandomForestClassifier_n-estimators_100_28.sav', 'rb') as file:
     model = pickle.load(file)
 
-# your code here
-st.title('Prediction:')
+# mapping
 
+
+st.title('Predicción:')
 
 feature1 = st.slider('Feature 1', min_value=..., max_value=...)
 feature2 = st.slider('Feature 2', min_value=..., max_value=...)
-# ...
+
 
 # Realiza la predicción con el modelo
-prediction = model.predict([[feature1, feature2, ...]])
+if st.button('Realizar Predicción'):
+    
+    prediction = model.predict([[feature1, feature2, ...]])
 
-st.write(f'Predicción: {prediction}')
+  
+    st.write(f'Predicción: {prediction}')
